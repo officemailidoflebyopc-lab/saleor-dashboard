e46532537a4f1b0b27ea4ab5ef7c791ba954ae81
OFFICE <officemailidoflebyopc@gmail.com>
2025-12-15 01:00:57 +0530

    Update UserAvatar.tsx

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -9,7 +9,7 @@ interface UserAvatarProps {
   scheme?: "accent1" | "transparent";
 }
 
-export const UserAvatar = ({ url, initials, scheme = "primary", ...rest }: UserAvatarProps) =>
+export const UserAvatar = ({ url, initials, scheme = "accent1", ...rest }: UserAvatarProps) =>
   url ? (
     <Avatar.User scheme={scheme} src={url} {...rest} />
   ) : (
