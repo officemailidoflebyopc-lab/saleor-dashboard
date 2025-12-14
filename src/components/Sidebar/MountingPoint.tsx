ae62521ceeafe30b5d1200eb48a1060f83b9a788
OFFICE <officemailidoflebyopc@gmail.com>
2025-12-14 23:16:42 +0530

    Update MountingPoint.tsx

--- src/components/Sidebar/MountingPoint.tsx
+++ src/components/Sidebar/MountingPoint.tsx
@@ -5,7 +5,7 @@ import { Avatar, Box, Text } from "@saleor/macaw-ui-next";
 
 export const MountingPoint = () => {
   const { theme } = useLegacyThemeHandler();
-  const logo = "https://static.wixstatic.com/media/28cd35_fc07dc978e294088911b1755e1cc078f~mv2.avif/v1/fill/w_192,h_246,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Lebyopc_logo_avif.avif";
+  const logo = "https://static.wixstatic.com/media/28cd35_fc07dc978e294088911b1755e1cc078f~mv2.avif/v1/fill/w_300,h_300,al_c,q_80,usm_0.66_1.00_0.01,enc_avif,quality_auto/Lebyopc_logo_avif.avif";
 
   return (
     <Box display="flex" gap={3} paddingX={4} paddingY={5} alignItems="center">
