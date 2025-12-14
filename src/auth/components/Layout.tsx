1927b78665cf8d0b1e235a2cf3769f9390da1e0c
OFFICE <officemailidoflebyopc@gmail.com>
2025-12-15 00:13:10 +0530

    Fix: Restore img tag for Lebyopc logo on login page

--- src/auth/components/Layout.tsx
+++ src/auth/components/Layout.tsx
@@ -86,6 +86,7 @@ const Layout = (props: { children: ReactNode }) => {
   return (
     <div className={classes.root}>
       <div className={classes.mainPanel}>
+                  <img className={classes.logo} src={lebyopcLogo} alt="Lebyopc Logo" />
         <div className={classes.mainPanelContent}>{children}</div>
       </div>
       <div className={classes.sidebar}>
